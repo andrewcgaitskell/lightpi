9c7ef5babea982d69b69dd20f67888e1304bd520
andrewcgaitskell <andrew@gaitskell.com>
2017-12-26 19:57:41 +0000

    Update btletest.py

--- btletest.py
+++ btletest.py
@@ -8,7 +8,7 @@ button_uuid = UUID("19B10012-E8F2-537E-4F6C-D104768A1214")
 p = Peripheral("98:4F:EE:0D:16:59", "random")
  
 try:
-    ch = p.getCharacteristics(uuid=tbutton_uuid)[0]
+    ch = p.getCharacteristics(uuid=button_uuid)[0]
     if (ch.supportsRead()):
         while 1:
             val = binascii.b2a_hex(ch.read())
