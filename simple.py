20c7b390c9d1e3ab6ace6b3f32457b9423c817de
andrewcgaitskell <andrew@gaitskell.com>
2017-12-26 21:49:00 +0000

    Update simple.py

--- simple.py
+++ simple.py
@@ -5,3 +5,10 @@ dev = btle.Peripheral("98:4F:EE:0D:16:59")
 print "Services..."
 for svc in dev.services:
     print str(svc)
+
+ButtonSensor = btle.UUID("19b10010-e8f2-537e-4f6c-d104768a1214")
+ 
+ButtonService = dev.getServiceByUUID(ButtonSensor)
+
+for ch in ButtonService.getCharacteristics():
+     print str(ch)
