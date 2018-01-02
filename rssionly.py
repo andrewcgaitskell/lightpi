c2e37715a7e01a3126281d6194f026c6dc8b2899
andrewcgaitskell <andrew@gaitskell.com>
2018-01-02 18:59:37 +0000

    Update rssionly.py

--- rssionly.py
+++ rssionly.py
@@ -5,5 +5,8 @@ import sys
 
 child = pexpect.spawn("bluetoothctl", echo = False)
 child.send("scan on" + "\n")
+for i in range(0, 10):
+    print(i)
+    time.sleep(1)
 print child.before.split("\r\n")
 
