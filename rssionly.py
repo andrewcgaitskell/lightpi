dfe5b18b1a2e582098841afd3825b8f1a2b9423a
andrewcgaitskell <andrew@gaitskell.com>
2018-01-02 19:07:58 +0000

    Update rssionly.py

--- rssionly.py
+++ rssionly.py
@@ -5,8 +5,14 @@ import sys
 
 child = pexpect.spawn("bluetoothctl", echo = False)
 child.send("scan on" + "\n")
+time.sleep(pause)
+start_failed = child.expect(["bluetooth", pexpect.EOF])
+if start_failed:
+    raise BluetoothctlError("Bluetoothctl failed after running " + command)
+out = child.before.split("\r\n")
 for i in range(0, 10):
     print(i)
     time.sleep(1)
-print child.before.split("\r\n")
+child.send("devices" + "\n")
+
 
